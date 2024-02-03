012c70e33e17b6ce52bab6d38c940a3823703d77
Lolopolko <lolopolkobrony@gmail.com>
2024-02-03 10:03:28 +0100

    v6

--- data.js
+++ data.js
@@ -1,140 +1,140 @@
 var data = [
     {
-        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
-        Category: 'Investment Form',
+        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Discord_logo_round.svg/120px-Discord_logo_round.svg.png?20240107134118" width="70" height="70">',
+        Category: 'community',
         Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
-        Description: '13 March, 2017',
+        Description: 'lies and slander !',
     
     },
     {
         Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Discord_logo_round.svg/120px-Discord_logo_round.svg.png?20240107134118" width="70" height="70">',
-        Category: 'Investment Form 2',
+        Category: 'community',
         Command: '<button onclick="copyText(this)" title="Click to copy">!cutedara</button>',
-        Description: '12 March, 2017',
+        Description: 'This is true ! ',
       
     },
     {
         Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
-        Category: 'Investment Form 3',
+        Category: 'fun',
+        Command: '<button onclick="copyText(this)" title="Click to copy">!sus</button>',
+        Description: 'Command will tell you how lewd you are today',
+       
+    },
+    {
+        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Discord_logo_round.svg/120px-Discord_logo_round.svg.png?20240107134118" width="70" height="70">',
+        Category: 'Images',
+        Command: '<button onclick="copyText(this)" title="Click to copy">!cat</button>',
+        Description: 'Command will show you picture of random cat',
+      
+    },
+    {
+        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
+        Category: 'video',
+        Command: '<button onclick="copyText(this)" title="Click to copy">!hotdog</button>',
+        Description: '10 March, 2017',
+       
+    },
+    {
+        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
+        Category: 'community',
+        Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
+        Description: '17 March, 2017',
+        
+    },
+    {
+        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
+        Category: 'community',
+        Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
+        Description: '10 March, 2017',
+        
+    },
+    {
+        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
+        Category: 'community',
+        Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
+        Description: '17 March, 2017',
+        
+    },
+    {
+        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
+        Category: 'community',
         Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
         Description: '10 March, 2017',
        
     },
     {
         Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
-        Category: 'Investment Form 4',
+        Category: 'community',
+        Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
+        Description: '17 March, 2017',
+       
+    },
+    {
+        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
+        Category: 'community',
+        Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
+        Description: '10 March, 2017',
+      
+    },
+    {
+        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
+        Category: 'community',
+        Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
+        Description: '17 March, 2017',
+       
+    },
+    {
+        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
+        Category: 'community',
+        Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
+        Description: '10 March, 2017',
+        
+    },
+    {
+        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
+        Category: 'community',
         Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
         Description: '17 March, 2017',
       
     },
     {
         Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
-        Category: 'Investment Form 3',
+        Category: 'community',
         Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
         Description: '10 March, 2017',
        
     },
     {
         Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
-        Category: 'Investment Form 4',
+        Category: 'community',
         Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
         Description: '17 March, 2017',
         
     },
     {
         Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
-        Category: 'Investment Form 3',
+        Category: 'community',
         Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
         Description: '10 March, 2017',
         
     },
     {
         Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
-        Category: 'Investment Form 4',
-        Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
-        Description: '17 March, 2017',
-        
-    },
-    {
-        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
-        Category: 'Investment Form 3',
-        Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
-        Description: '10 March, 2017',
-       
-    },
-    {
-        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
-        Category: 'Investment Form 4',
+        Category: 'community',
         Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
         Description: '17 March, 2017',
        
     },
     {
         Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
-        Category: 'Investment Form 3',
-        Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
-        Description: '10 March, 2017',
-      
-    },
-    {
-        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
-        Category: 'Investment Form 4',
-        Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
-        Description: '17 March, 2017',
-       
-    },
-    {
-        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
-        Category: 'Investment Form 3',
+        Category: 'community',
         Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
         Description: '10 March, 2017',
         
     },
     {
         Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
-        Category: 'Investment Form 4',
-        Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
-        Description: '17 March, 2017',
-      
-    },
-    {
-        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
-        Category: 'Investment Form 3',
-        Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
-        Description: '10 March, 2017',
-       
-    },
-    {
-        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
-        Category: 'Investment Form 4',
-        Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
-        Description: '17 March, 2017',
-        
-    },
-    {
-        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
-        Category: 'Investment Form 3',
-        Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
-        Description: '10 March, 2017',
-        
-    },
-    {
-        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
-        Category: 'Investment Form 4',
-        Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
-        Description: '17 March, 2017',
-       
-    },
-    {
-        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
-        Category: 'Investment Form 3',
-        Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
-        Description: '10 March, 2017',
-        
-    },
-    {
-        Platform: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/103px-Twitch_Glitch_Logo_Purple.svg.png?20200520095038" width="65" height="70">',
-        Category: 'Investment Form 4',
+        Category: 'community',
         Command: '<button onclick="copyText(this)" title="Click to copy">!cutopolko</button>',
         Description: '17 March, 2017',
         
